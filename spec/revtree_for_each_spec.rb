84d777273fb76150d7430ca8c16fa09b53efb4f5
Julian Kahlert <90937526+juliankahlert@users.noreply.github.com>
2024-09-06 22:38:20 +0200

    spec: Add tests
    Signed-off-by: Julian Kahlert <90937526+juliankahlert@users.noreply.github.com>

---
+++ spec/revtree_for_each_spec.rb
@@ -0,0 +1,115 @@
+# spec/revtree_for_each_spec.rb
+#
+# MIT License
+#
+# Copyright (c) 2024 Julian Kahlert
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'spec_helper'
+require 'revtree'
+
+RSpec.describe RevTree do
+  let(:file_structure) do
+    {
+      type: :folder,
+      name: 'root',
+      rev: 'rootrev',
+      status: :modified,
+      children: [
+        {
+          type: :file,
+          name: 'file1.rb',
+          rev: 'file1rev',
+          status: :added,
+          children: [
+
+          ]
+        },
+        {
+          type: :file,
+          name: 'file2.md',
+          rev: 'file2rev',
+          status: :removed,
+          children: [
+
+          ]
+        },
+        {
+          type: :folder,
+          name: 'subfolder',
+          rev: 'subfolderrev',
+          status: :unmodified,
+          children: [
+            {
+              type: :file,
+              name: 'file3.rb',
+              rev: 'file3rev',
+              status: :unmodified,
+              children: [
+
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  end
+
+  let(:revtree) { RevTree.from_h(file_structure) }
+
+  describe '#for_each' do
+    it 'calls the block for files with statuses in the whitelist' do
+      statuses = [:added, :removed]
+      results = []
+      revtree.for_each(statuses) do |file, full_path|
+        results << { name: file.name, status: file.status }
+      end
+
+      expect(results).to contain_exactly(
+        { name: 'file1.rb', status: :added },
+        { name: 'file2.md', status: :removed }
+      )
+    end
+
+    it 'does not call the block for files with statuses not in the whitelist' do
+      statuses = [:added]
+      results = []
+
+      revtree.for_each(statuses) do |file, full_path|
+        results << { name: file.name, status: file.status }
+      end
+
+      expect(results).to contain_exactly(
+        { name: 'file1.rb', status: :added }
+      )
+    end
+
+    it 'handles an empty status whitelist' do
+      statuses = []
+      results = []
+
+      revtree.for_each(statuses) do |file, full_path|
+        results << { name: file.name, status: file.status }
+      end
+
+      expect(results).to be_empty
+    end
+  end
+end
---
+++ spec/revtree_spec.rb
@@ -0,0 +1,348 @@
+# spec/revtree_spec.rb
+#
+# MIT License
+#
+# Copyright (c) 2024 Julian Kahlert
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'rspec'
+require_relative '../lib/revtree'
+
+RSpec.describe RevTree do
+  let(:old_tree_json) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:new_tree_json_added) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file3.txt",
+            "rev": "revfile3new",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:new_tree_json_removed) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:new_tree_json_modified) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1new",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:new_tree_json_unmodified) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:expected_tree_json_added) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file3.txt",
+            "rev": "revfile3new",
+            "status": "added",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:expected_tree_json_removed) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "removed",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:expected_tree_json_modified) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "modified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1new",
+            "status": "modified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  let(:expected_tree_json_unmodified) do
+    <<~JSON
+      {
+        "type": "folder",
+        "name": "root",
+        "rev": "abcd1234",
+        "status": "unmodified",
+        "children": [
+          {
+            "type": "file",
+            "name": "file1.txt",
+            "rev": "revfile1",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          },
+          {
+            "type": "file",
+            "name": "file2.txt",
+            "rev": "revfile2",
+            "status": "unmodified",
+            "children": [
+
+            ]
+          }
+        ]
+      }
+    JSON
+  end
+
+  it 'marks added files correctly' do
+    old_tree = RevTree.from_json(old_tree_json)
+    new_tree = RevTree.from_json(new_tree_json_added)
+    result_tree = RevTree.compare(old_tree, new_tree)
+    result_json = result_tree.to_json
+    expect(result_json).to eq(expected_tree_json_added.strip)
+  end
+
+  it 'marks removed files correctly' do
+    old_tree = RevTree.from_json(old_tree_json)
+    new_tree = RevTree.from_json(new_tree_json_removed)
+    result_tree = RevTree.compare(old_tree, new_tree)
+    result_json = result_tree.to_json
+    expect(result_json).to eq(expected_tree_json_removed.strip)
+  end
+
+  it 'marks modified files correctly' do
+    old_tree = RevTree.from_json(old_tree_json)
+    new_tree = RevTree.from_json(new_tree_json_modified)
+    result_tree = RevTree.compare(old_tree, new_tree)
+    result_json = result_tree.to_json
+    expect(result_json).to eq(expected_tree_json_modified.strip)
+  end
+
+  it 'marks unmodified files correctly' do
+    old_tree = RevTree.from_json(old_tree_json)
+    new_tree = RevTree.from_json(new_tree_json_unmodified)
+    result_tree = RevTree.compare(old_tree, new_tree)
+    result_json = result_tree.to_json
+    expect(result_json).to eq(expected_tree_json_unmodified.strip)
+  end
+end
